faf3b99316a76382b46a7b0404b2d821d238f47b
Chappers <116145040+lachappers@users.noreply.github.com>
2023-03-17 15:26:46 +0000

    added nested profile params to sign up by configuring devise registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,22 @@
 Rails.application.routes.draw do
-  get 'profiles/new'
-  get 'profiles/create'
-  get 'profiles/show'
-  get 'profiles/index'
-  get 'profiles/edit'
-  get 'profiles/update'
-  get 'profiles/destroy'
-  get 'like/create'
-  get 'like/destroy'
-  get 'comments/index'
-  get 'comments/show'
-  get 'comments/create'
-  get 'comments/new'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/create'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  get 'static_pages/root'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "static_pages#root"
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  # resources :users, only: %i[index show] do
+  #   resources :friendships, only: %i[create]
+  # end
+
+  resources :posts do
+    resources :comments, only: [:create]
+  end
+  resources :likes, only: [:create, :destroy]
+  resources :profiles, only: :show
+  # resources :friendships, only: [:create, :destroy]
+  resource :static_pages, only: :show
+
 end

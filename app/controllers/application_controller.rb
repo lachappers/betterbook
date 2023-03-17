a347890fae53eca6cfd1f3da567460cfc9b2354e
Chappers <116145040+lachappers@users.noreply.github.com>
2023-03-17 15:45:19 +0000

    added nested profile params to registration view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    layout :layout_by_resource
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    # redirect user to login page after signout
+    def after_sign_out_path_for(resource_or_scope)
+        new_user_session_path
+    end
+
+    private
+
+    # if directed to view managed by devise (login), render that layout, not application
+    def layout_by_resource
+        if devise_controller?
+        "devise"
+        else
+        "application"
+        end
+    end
+
+    protected
+
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :profile_image])
+    end
 end

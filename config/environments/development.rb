715d1dd0b6a519672e11d6e8fd11205066d4ff13
Chappers <116145040+lachappers@users.noreply.github.com>
2023-03-24 11:44:43 +0000

    Revert "added stimulus text box reset on submit"
    This reverts commit adbd88c72f206e2d373ea69d1844ab341b50f727.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,9 +54,7 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
-  # config.active_record.verbose_query_logs = true
-
-  config.active_record.verbose_query_logs = false
+  config.active_record.verbose_query_logs = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
